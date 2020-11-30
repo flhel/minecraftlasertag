f738ed116cc10eba3bfb61affe235d67d0b860da
flhel <58229462+flhel@users.noreply.github.com>
2020-11-30 18:47:17 +0100

    0.8

---
+++ src/main/java/lasertag/client/entity/model/Modelshoot.java
@@ -0,0 +1,36 @@
+package lasertag.client.entity.model;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+
+import net.minecraft.client.renderer.entity.model.EntityModel;
+import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.entity.Entity;
+
+public class Modelshoot extends EntityModel<Entity> {
+	private final ModelRenderer bb_main;
+	public Modelshoot() {
+		textureWidth = 16;
+		textureHeight = 16;
+		bb_main = new ModelRenderer(this);
+		bb_main.setRotationPoint(0.0F, 24.0F, 0.0F);
+		bb_main.setTextureOffset(0, 0).addBox(-39.5F, -10.0F, -7.5F, 40.0F, 10.0F, 10.0F, 0.0F, false);
+	}
+
+	@Override
+	public void setRotationAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+		// previously the render function, render code was moved to a method below
+	}
+
+	@Override
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue,
+			float alpha) {
+		bb_main.render(matrixStack, buffer, packedLight, packedOverlay);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
+}
