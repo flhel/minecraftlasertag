5120a774e13fd8c21257d7e3e0c5517698ef8b6e
flhel <58229462+flhel@users.noreply.github.com>
2021-01-16 16:01:49 +0100

    .

--- src/main/java/lasertag/armor/ModArmorMaterial.java
+++ src/main/java/lasertag/armor/ModArmorMaterial.java
@@ -4,18 +4,13 @@ import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.SoundEvent;
-import net.minecraft.util.SoundEvents;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraft.item.Items;
 
 import java.util.function.Supplier;
 
-public enum ModArmorMaterial implements IArmorMaterial {
-	
-	VESTONE ("lasertag:lasertagvestone", 33, new int[] {0, 0, 0, 0},  0,
-			SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0.0F, () -> {return Ingredient.fromItems(Items.IRON_INGOT);});
-	
+public class ModArmorMaterial implements IArmorMaterial {
+
 	//private static final int[] MAX_DAMAGE_ARRAY = new int[] {0, 0, 0, 0};
 	private final String name;
 	//private final int maxDamageFactor;
@@ -25,7 +20,7 @@ public enum ModArmorMaterial implements IArmorMaterial {
 	private final float toughness;
 	private final Supplier<Ingredient> repairMaterial;
 	
-	ModArmorMaterial(String name, int maxDamageFactor, int[] damageReductionAmountArray, int enchantability, SoundEvent soundEvent, float toughness, Supplier<Ingredient> repairMaterial) {
+	public ModArmorMaterial(String name, int maxDamageFactor, int[] damageReductionAmountArray, int enchantability, SoundEvent soundEvent, float toughness, Supplier<Ingredient> repairMaterial) {
 		this.name = name;
 		//this.maxDamageFactor = maxDamageFactor;
 		//this.damageReductionAmountArray = damageReductionAmountArray;
--- src/main/java/lasertag/item/ModItems.java
+++ src/main/java/lasertag/item/ModItems.java
@@ -2,24 +2,38 @@ package lasertag.item;
 
 import lasertag.Utils;
 import lasertag.armor.ModArmorMaterial;
-import lasertag.armor.ModArmorMaterial2;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Items;
 import net.minecraft.item.SwordItem;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.SoundEvents;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 
-public class ModItems {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Utils.MOD_ID);
-    public static final RegistryObject<Phaser> PHASER = ITEMS.register("phaser", Phaser::new);
-    public static final RegistryObject<LaserstrahlItem> LASERSTRAHL_ITEM = ITEMS.register("laserstrahl_item", LaserstrahlItem::new );
-    public static final RegistryObject<ArmorItem> LASERTAGVESTONE = ITEMS.register("lasertagvestone",
-			() -> new ArmorItem(ModArmorMaterial.VESTONE, EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)));
+public class ModItems {  
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Utils.MOD_ID);
+	
+	//Armor
+	public static final RegistryObject<ArmorItem> LASERTAGVESTONE = ITEMS.register("lasertagvestone",
+			() -> new ArmorItem(new ModArmorMaterial("lasertag:lasertagvestone", 33, new int[] {0, 0, 0, 0},  0,
+					SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0.0F, () -> {return Ingredient.fromItems(Items.IRON_INGOT);}),
+					EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)));
+	
 	public static final RegistryObject<ArmorItem> LASERTAGVESTTWO = ITEMS.register("lasertagvesttwo",
-			() -> new ArmorItem(ModArmorMaterial2.VESTTWO, EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)));
-	public static final RegistryObject<SwordItem> KNIVE = ITEMS.register("knive", () -> new SwordItem(Knive.KNIVE , 4, 1.0f, new Item.Properties().group(ItemGroup.COMBAT)));
+			() -> new ArmorItem(new ModArmorMaterial("lasertag:lasertagvesttwo", 33, new int[] {0, 0, 0, 0},  0,
+					SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0.0F, () -> {return Ingredient.fromItems(Items.IRON_INGOT);}),
+					EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)));
+	//Phasers    
+	public static final RegistryObject<Phaser> PHASERRED = ITEMS.register("phaser_red", PhaserRed::new);
+	public static final RegistryObject<Phaser> PHASERBLUE = ITEMS.register("phaser_blue", PhaserBlue::new);
+    //Knives
+	public static final RegistryObject<SwordItem> KNIVERED = ITEMS.register("knive_red", () -> new SwordItem(Knive.KNIVE , 4, 1.0f, new Item.Properties().group(ItemGroup.COMBAT)));
+	public static final RegistryObject<SwordItem> KNIVEBLUE = ITEMS.register("knive_blue", () -> new SwordItem(Knive.KNIVE , 4, 1.0f, new Item.Properties().group(ItemGroup.COMBAT)));
+	//Ammo
+	public static final RegistryObject<LaserstrahlItem> LASERSTRAHL_ITEM = ITEMS.register("laserstrahl_item", LaserstrahlItem::new );
 }
