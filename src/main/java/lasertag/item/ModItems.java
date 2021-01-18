9ea86276231ae3f9adf9590d79bbaa75b9488ab7
flhel <58229462+flhel@users.noreply.github.com>
2021-01-18 18:49:15 +0100

    .

--- src/main/java/lasertag/item/ModItems.java
+++ src/main/java/lasertag/item/ModItems.java
@@ -33,8 +33,8 @@ public class ModItems {
 	public static final RegistryObject<MgRed> MGRED = ITEMS.register("mg_red", MgRed::new);
 	public static final RegistryObject<MgBlue> MGBLUE = ITEMS.register("mg_blue", MgBlue::new);
     //Knives
-	//public static final RegistryObject<SwordItem> KNIVERED = ITEMS.register("knive_red", () -> new SwordItem(Knive.KNIVE, 4, 1.0f, new Item.Properties().group(ItemGroup.COMBAT)));
-	//public static final RegistryObject<SwordItem> KNIVEBLUE = ITEMS.register("knive_blue", () -> new SwordItem(Knive.KNIVE , 4, 1.0f, new Item.Properties().group(ItemGroup.COMBAT)));
+	//public static final RegistryObject<SwordItem> KNIVERED = ITEMS.register("lasertag:knive_red", () -> new SwordItem(Knive.KNIVE, 4, 1.0f, new Item.Properties().group(ItemGroup.COMBAT)));
+	//public static final RegistryObject<SwordItem> KNIVEBLUE = ITEMS.register("lasertag:knive_blue", () -> new SwordItem(Knive.KNIVE , 4, 1.0f, new Item.Properties().group(ItemGroup.COMBAT)));
 	//Ammo
 	public static final RegistryObject<LaserstrahlItem> LASERSTRAHL_ITEM = ITEMS.register("laserstrahl_item", LaserstrahlItem::new);
 	//Potions
