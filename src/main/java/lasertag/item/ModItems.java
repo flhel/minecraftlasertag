d4d20fa025591e874c55a8ec91ad9ab065421729
Chaotrax <74452019+Chaotrax@users.noreply.github.com>
2021-01-19 18:06:20 +0100

    new textures, new armor

--- src/main/java/lasertag/item/ModItems.java
+++ src/main/java/lasertag/item/ModItems.java
@@ -25,10 +25,26 @@ public class ModItems {
 					SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0.0F, () -> {return Ingredient.fromItems(Items.IRON_INGOT);}),
 					EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)));
 	
+	public static final RegistryObject<ArmorItem> LASERTAGVESTONE_HELMET = ITEMS.register("lasertagvestonehelmet",
+			() -> new ArmorItem(new CybertagArmor("lasertag:lasertagvestone", 33, new int[] {3, 3, 3, 3},  0,
+					SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0.0F, () -> {return Ingredient.fromItems(Items.IRON_INGOT);}),
+					EquipmentSlotType.HEAD, new Item.Properties().group(ItemGroup.COMBAT)));
+	
+	public static final RegistryObject<ArmorItem> LASERTAGVESTONE_LEGS = ITEMS.register("lasertagvestonelegs",
+			() -> new ArmorItem(new CybertagArmor("lasertag:lasertagvestone", 33, new int[] {2, 2, 2, 2},  0,
+					SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0.0F, () -> {return Ingredient.fromItems(Items.IRON_INGOT);}),
+					EquipmentSlotType.LEGS, new Item.Properties().group(ItemGroup.COMBAT)));
+	
+	public static final RegistryObject<ArmorItem> LASERTAGVESTONE_SHOES= ITEMS.register("lasertagvestoneshoes",
+			() -> new ArmorItem(new CybertagArmor("lasertag:lasertagvestone", 33, new int[] {1, 1, 1, 1},  0,
+					SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0.0F, () -> {return Ingredient.fromItems(Items.IRON_INGOT);}),
+					EquipmentSlotType.FEET, new Item.Properties().group(ItemGroup.COMBAT)));
+	
 	public static final RegistryObject<ArmorItem> LASERTAGVESTTWO = ITEMS.register("lasertagvesttwo",
 			() -> new ArmorItem(new CybertagArmor("lasertag:lasertagvesttwo", 33, new int[] {0, 0, 0, 0},  0,
 					SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0.0F, () -> {return Ingredient.fromItems(Items.IRON_INGOT);}),
 					EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)));
+	
 	//Guns 
 	public static final RegistryObject<PhaserRed> PHASERRED = ITEMS.register("phaser_red", PhaserRed::new);
 	public static final RegistryObject<PhaserBlue> PHASERBLUE = ITEMS.register("phaser_blue", PhaserBlue::new);
