3b2fab337903e3ac9d41cfc64d7c08cf97279ab8
flhel <58229462+flhel@users.noreply.github.com>
2021-01-20 13:18:21 +0100

    .

--- src/main/java/lasertag/armor/CybertagArmor.java
+++ src/main/java/lasertag/armor/CybertagArmor.java
@@ -14,16 +14,16 @@ public class CybertagArmor implements IArmorMaterial {
 	//private static final int[] MAX_DAMAGE_ARRAY = new int[] {0, 0, 0, 0};
 	private final String name;
 	//private final int maxDamageFactor;
-	//private final int[] damageReductionAmountArray;
+	private final int[] damageReductionAmountArray;
 	private final int enchantability;
 	private final SoundEvent soundEvent;
 	private final float toughness;
 	private final Supplier<Ingredient> repairMaterial;
 	
-	public CybertagArmor(String name, int maxDamageFactor, int[] damageReductionAmountArray, int enchantability, SoundEvent soundEvent, float toughness, Supplier<Ingredient> repairMaterial) {
+	public CybertagArmor(String name, int maxDamageFactor, int[] damageReductionAmountArrayIn, int enchantability, SoundEvent soundEvent, float toughness, Supplier<Ingredient> repairMaterial) {
 		this.name = name;
 		//this.maxDamageFactor = maxDamageFactor;
-		//this.damageReductionAmountArray = damageReductionAmountArray;
+		this.damageReductionAmountArray = damageReductionAmountArrayIn;
 		this.enchantability = enchantability;
 		this.soundEvent = soundEvent;
 		this.toughness = toughness;
@@ -38,8 +38,7 @@ public class CybertagArmor implements IArmorMaterial {
 
 	@Override
 	public int getDamageReductionAmount(EquipmentSlotType slotIn) {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.damageReductionAmountArray[slotIn.getIndex()];
 	}
 
 	@Override
